bdd9177857bbba9e228f06e13b89031cf3ded771
Dominik Romero <0715327008@s.tusd1.org>
2026-03-03 18:34:48 +0000

    day2

--- script.js
+++ script.js
@@ -12,10 +12,20 @@ clickDisplay.innerText = "Clicks: " + clicks;
 // Called when the potato is clicked
 function handleClick() {
     console.log("click");
-}
 
     //Add one to click count
     clicks = clicks + 1;
 
     //Update display
     clickDisplay.innerText = "Clicks:" + clicks;
+
+    // Detect if they reach 10 clicks
+    if (clicks == 10){
+        alert("Congrats you reached level 2 you get 2 per clicks now ");
+
+        
+    }
+
+}
+
+    
